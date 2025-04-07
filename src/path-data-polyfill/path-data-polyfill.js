8f97f9ee1dddebdd95a9ba02cac4d342a30f7342
Lorenzo Nizzi <lorenzo@infornix.it>
2025-04-07 16:18:54 +0200

    Update version to 1.0.8 and enhance Firefox compatibility in path-data-polyfill

--- src/path-data-polyfill/path-data-polyfill.js
+++ src/path-data-polyfill/path-data-polyfill.js
@@ -23,7 +23,7 @@ function pathDataPolyfill(window) {
 //   Jarosław Foksa
 // @license
 //   MIT License
-if (!window.SVGPathElement.prototype.getPathData || !window.SVGPathElement.prototype.setPathData) {
+if (!window.SVGPathElement.prototype.getPathData || !window.SVGPathElement.prototype.setPathData || navigator.userAgent.indexOf("Firefox/") > -1) {
   (function() {
     var commandsMap = {
       "Z":"Z", "M":"M", "L":"L", "C":"C", "Q":"Q", "A":"A", "H":"H", "V":"V", "S":"S", "T":"T",
